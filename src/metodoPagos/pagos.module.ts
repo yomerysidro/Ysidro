113752ab533fe88b7fec7b91fedd7bbeb599fd64
YomerYsidro <ysidrosy@gmail.com>
2024-11-19 08:14:23 -0500

    In Process

--- src/metodoPagos/pagos.module.ts
+++ src/metodoPagos/pagos.module.ts
@@ -4,11 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PagosService } from './pagos.service';
 import { PagosController } from './pagos.controller';
 import { Pago } from './pago.entity';
-import { TokenService } from './token.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Pago])],
-  providers: [PagosService, TokenService],
+  providers: [PagosService],
   controllers: [PagosController],
   exports: [TypeOrmModule],
 })
