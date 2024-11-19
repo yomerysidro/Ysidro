863486922357e7c24fb5059aac4c0d37b8761a5b
YomerYsidro <ysidrosy@gmail.com>
2024-11-18 23:27:31 -0500

    BD

--- src/metodoPagos/mercadoPago.config.ts
+++ src/metodoPagos/mercadoPago.config.ts
@@ -1,4 +1,14 @@
-import MercadoPago from 'mercadopago';
+import { MercadoPagoConfig } from 'mercadopago/dist/mercadoPagoConfig';
+import type { Config } from 'mercadopago/dist/types';
 
-// Configurar el access token en modo prueba
-MercadoPago.configurations.setAccessToken('TEST-5456723910567615-110411-fb6d6bc8fb0c5dbf7be728a1e923b2ca-1741789467');
+const config: Config = {
+  accessToken: 'TEST-5456723910567615-110411-fb6d6bc8fb0c5dbf7be728a1e923b2ca-1741789467',
+  options: {
+    timeout: 5000, // Opcional: Configura un tiempo de espera para las solicitudes
+  },
+};
+
+const mercadoPagoInstance = new MercadoPagoConfig(config);
+
+// Ahora puedes usar mercadoPagoInstance con tu configuración.
+console.log(mercadoPagoInstance.accessToken); // Verifica que el token se haya configurado
