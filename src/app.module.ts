32f9c396f096c49652e829bb1520ec1ae6ce3a6a
YomerYsidro <ysidrosy@gmail.com>
2024-11-19 09:31:13 -0500

    Sera Render

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,21 +10,23 @@ import { ReservasModule } from './reservas/reservas.module';
 import { PagosModule } from './metodoPagos/pagos.module';
 import { DetalleReservaModule } from './detallesReservas/detalleReserva.module';
 import { HabitacionesModule } from './habitaciones/habitaciones.module';
+import * as dotenv from 'dotenv';
 
-
-
+// Cargar las variables de entorno desde un archivo .env
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'yomer',
-      database: 'leona',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      type: 'mysql', // Tipo de base de datos
+      host: process.env.DB_HOST || 'localhost', // Host (usa variable de entorno o 'localhost' por defecto)
+      port: parseInt(process.env.DB_PORT, 10) || 3306, // Puerto (usa variable de entorno o 3306 por defecto)
+      username: process.env.DB_USERNAME || 'root', // Usuario
+      password: process.env.DB_PASSWORD || 'yomer', // Contraseña
+      database: process.env.DB_NAME || 'leona', // Nombre de la base de datos
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Rutas a las entidades
+      synchronize: process.env.TYPEORM_SYNC === 'true' || true, // Sincroniza el esquema (desactiva en producción)
+      logging: process.env.TYPEORM_LOGGING === 'true' || true, // Habilita logs de depuración
     }),
     UsersModule,
     AuthModule,
