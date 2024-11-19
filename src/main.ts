efe553c495e57e8fb2144b9f22259b2bd95a3263
YomerYsidro <ysidrosy@gmail.com>
2024-11-19 09:39:43 -0500

    render

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {ValidationPipe} from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false })); //activar las validaciones
-  await app.listen(3000, '192.168.56.1' );
+
+  // Habilitar validaciones globales
+  app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
+
+  // Leer el puerto de las variables de entorno o usar un valor predeterminado
+  const PORT = process.env.PORT || 3000;
+
+  // Escuchar en cualquier interfaz (importante para Render)
+  await app.listen(PORT, '0.0.0.0');
+
+  console.log(`Application is running on: http://localhost:${PORT}`);
 }
 bootstrap();
