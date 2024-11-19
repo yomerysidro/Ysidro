b4a9f3eeb964351134bd49505e85b131925927dc
YomerYsidro <ysidrosy@gmail.com>
2024-11-19 15:53:22 -0500

    Backend...

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,17 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); // Cargar las variables de entorno del archivo .env
+dotenv.config(); // Cargar las variables de entorno
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      url: process.env.MYSQL_URL,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: process.env.TYPEORM_SYNC === 'true',
-      logging: process.env.TYPEORM_LOGGING === 'true',
-    }),    
+      type: 'mysql', // Tipo de base de datos
+      url: 'mysql://root:wDRkXpjDddKXBkDTKZayefGUElAhxLGe@autorack.proxy.rlwy.net:14983/railway',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // Ajustar según la estructura del proyecto
+      synchronize: process.env.TYPEORM_SYNC === 'true', // Sincronización automática de entidades (solo en desarrollo)
+      logging: process.env.TYPEORM_LOGGING === 'true', // Habilitar logs si es necesario
+    }),
   ],
 })
 export class AppModule {}
